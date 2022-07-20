e9bf639559e5c463d01818b58b5ea61e88a85a53
GorbatayaGora <senia.azatkhanov@gmail.com>
2022-07-20 05:48:54 +0300

    Ещё потыкал, туплю

--- Danya Lox/Assets/Tests/Men/PlayerAnim.cs
+++ Danya Lox/Assets/Tests/Men/PlayerAnim.cs
@@ -23,26 +23,20 @@ public class PlayerAnim : MonoBehaviour
     void Update()
     {
 
-        targetSkewPos = defaultSkewPos + skewedInput2 * skewPower;
+        
         var input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")); // Инпути осей окда
 
-        var _isoMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, Mathf.DeltaAngle(playerController.transform.rotation.eulerAngles.y, playerCamera.mouseObj.transform.rotation.eulerAngles.y) + skewAngle, 0));
+        //var _isoMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, Mathf.DeltaAngle(transform.rotation.eulerAngles.y, targetDot.transform.rotation.eulerAngles.y) + skewAngle, 0));
+        var _isoMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, skewAngle, 0));
         skewedInput2 = _isoMatrix.MultiplyPoint3x4(input);
 
+
+        targetSkewPos = defaultSkewPos + skewedInput2 * skewPower;
         targetDot.transform.localPosition = Vector3.MoveTowards(targetDot.transform.localPosition, targetSkewPos, skewSpeed * 0.01f);
 
 
-
-
-
-
-        //targetDot.transform.localPosition = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        //var _isoMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, Mathf.DeltaAngle(playerController.transform.rotation.eulerAngles.y, targetDot.transform.rotation.eulerAngles.y) + skewAngle, 0));
-        //var skewedInput = _isoMatrix.MultiplyPoint3x4(playerController.transform.position - targetDot.transform.position); // Переменная, которая хранит сдвинутий інпут
-        //print(skewedInput);
-        //anim.SetFloat("BlendX", skewedInput.x);
-        //anim.SetFloat("BlendY", skewedInput.z);
-
-
+        Ray ray1 = new Ray(playerController.transform.position, playerCamera.mouseObj.transform.position);
+        Ray ray2 = new Ray(playerController.transform.position, playerController.transform.forward);
+        print(Vector3.SignedAngle(ray1.direction, ray2.direction, Vector3.up));
     }
 }
