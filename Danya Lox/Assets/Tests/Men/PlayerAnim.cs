746d0c8183d79bc81e90e800a00e45cc5b5efba9
GorbatayaGora <senia.azatkhanov@gmail.com>
2022-07-23 04:15:42 +0300

    Контроллер всё
    Доебенил контроллер, осталось камеру короче даня даня.
    Там два способа ходьбы данилский и нормальный на О(Щ) переключается понял да.

--- Danya Lox/Assets/Tests/Men/PlayerAnim.cs
+++ Danya Lox/Assets/Tests/Men/PlayerAnim.cs
@@ -4,30 +4,50 @@ using UnityEngine;
 
 public class PlayerAnim : MonoBehaviour
 {
+    Vector3 vectorCord, skewedInput;
     Animator animator; 
     int targetXCord, targetYCord;
     float XCord, YCord;
     public bool isMoving;
-    [SerializeField] float animSwapSpeed;
-    // Start is called before the first frame update
+    [SerializeField] float animSwapSpeed, skewAngle;
+    PlayerController playerController;
     void Start()
     {
+        playerController = FindObjectOfType<PlayerController>();
         animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        targetXCord = (int)Input.GetAxisRaw("Horizontal");
-        targetYCord = (int)Input.GetAxisRaw("Vertical");
+        if(playerController.trigger)
+        {
+            targetXCord = (int)Input.GetAxisRaw("Horizontal");
+            targetYCord = (int)Input.GetAxisRaw("Vertical");
 
-        XCord = Mathf.MoveTowards(XCord, targetXCord, animSwapSpeed * 0.01f);
-        YCord = Mathf.MoveTowards(YCord, targetYCord, animSwapSpeed * 0.01f);
+            XCord = Mathf.MoveTowards(XCord, targetXCord, animSwapSpeed * 0.01f);
+            YCord = Mathf.MoveTowards(YCord, targetYCord, animSwapSpeed * 0.01f);
 
-        isMoving = new Vector2(targetXCord,targetYCord).magnitude != 0 ? true : false;
+            isMoving = new Vector2(targetXCord, targetYCord).magnitude != 0 ? true : false;
 
-        animator.SetBool("isMov", isMoving);
-        animator.SetFloat("BlendX", XCord);
-        animator.SetFloat("BlendY", YCord);
+            animator.SetBool("isMov", isMoving);
+            animator.SetFloat("BlendX", XCord);
+            animator.SetFloat("BlendY", YCord);
+        }
+        else
+        {
+            vectorCord = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+            var _isoMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, -Mathf.Abs(transform.rotation.eulerAngles.y) + skewAngle, 0));
+
+            skewedInput = _isoMatrix.MultiplyPoint3x4(vectorCord);
+
+            XCord = Mathf.MoveTowards(XCord, skewedInput.x, animSwapSpeed * 0.01f);
+            YCord = Mathf.MoveTowards(YCord, skewedInput.z, animSwapSpeed * 0.01f);
+
+            isMoving = skewedInput.magnitude != 0 ? true : false;
+
+            animator.SetBool("isMov", isMoving);
+            animator.SetFloat("BlendX", XCord);
+            animator.SetFloat("BlendY", -YCord);
+        }    
     }
 }
--- Danya Lox/Assets/Tests/Men/PlayerController.cs
+++ Danya Lox/Assets/Tests/Men/PlayerController.cs
@@ -4,38 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Vector3 mousePos;
+    public Vector3 mousePos, vectorInput, skewedInput;
 
-    float xInput, yInput, xMovement, yMovement;
-    Rigidbody rb;
+    float xInput, yInput;
+
     [SerializeField] Camera cam;
     [SerializeField] float movementSpeed, rotationSpeed;
     [SerializeField] LayerMask layerMask;
     [SerializeField] float playerAndCursorAngle;
+    public bool trigger = false;
     Transform playerLook;
 
     private const float DegToRad = Mathf.PI / 180;
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
         playerLook = transform.GetChild(0);
     }
 
     void Update()
     {
-        
-
+        if (Input.GetKeyDown(KeyCode.O))
+            trigger = !trigger;
         xInput = Input.GetAxisRaw("Horizontal");
         yInput = Input.GetAxisRaw("Vertical");
-
-
+        vectorInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0 , Input.GetAxisRaw("Vertical"));
 
         PlayerRotation();
         PointerPosition();
-        PlayerMovement();
-    }
 
+        if (trigger)
+            PlayerMovement();
+        else
+            PlayerMovement2();
+    }
 
 
     void PlayerRotation()
@@ -57,11 +59,20 @@ public class PlayerController : MonoBehaviour
     {
         if (yInput == 0 && xInput == 0) return;
         else if (yInput != 0)
-            transform.Translate((mousePos - transform.position) * yInput * movementSpeed * Time.deltaTime / Vector3.Distance(transform.position, mousePos));
+            transform.Translate((mousePos - transform.position).normalized * yInput * movementSpeed * Time.deltaTime );
         else
-            transform.Translate(( RotateVector(mousePos - transform.position, 90)) * -xInput * movementSpeed * Time.deltaTime / Vector3.Distance(transform.position, mousePos));
+            transform.Translate(( RotateVector(mousePos - transform.position, 90)).normalized * -xInput * movementSpeed * Time.deltaTime );
+    }  
+    
+    void PlayerMovement2()
+    {
+        var _isoMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
+        skewedInput = _isoMatrix.MultiplyPoint3x4(vectorInput);
 
+        if (vectorInput.magnitude != 0)
+            transform.Translate(skewedInput * Time.deltaTime * movementSpeed);
     }
+
 
     Vector3 RotateVector(Vector3 defaultVector ,int degrees)
     {
